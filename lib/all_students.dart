650d8d224ab9697958e18f722d6f7f7d25c81b7a
Ahammed Hinas <82311707+hinasahammed@users.noreply.github.com>
2024-05-28 19:35:21 +0530

    added firebase

--- lib/all_students.dart
+++ lib/all_students.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class AllStudents extends StatelessWidget {
   const AllStudents({super.key});
@@ -14,14 +15,59 @@ class AllStudents extends StatelessWidget {
           vertical: 10,
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
-              'All  ,\nStudents',
+              'All,\nStudents',
               style: theme.textTheme.headlineLarge!.copyWith(
                 color: theme.colorScheme.onPrimaryContainer,
                 fontWeight: FontWeight.w600,
               ),
             ),
+            const Gap(20),
+            for (var i = 0; i <= 5; i++)
+              Card(
+                child: Container(
+                  width: double.infinity,
+                  padding: const EdgeInsets.all(12),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Ajay devgn',
+                            style: theme.textTheme.titleLarge!.copyWith(
+                              color: theme.colorScheme.onSurface,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            'DOB: 10-10-2024',
+                            style: theme.textTheme.labelLarge!.copyWith(
+                              color: theme.colorScheme.onSurface,
+                            ),
+                          ),
+                          Text(
+                            'Gender: Male',
+                            style: theme.textTheme.labelLarge!.copyWith(
+                              color: theme.colorScheme.onSurface,
+                            ),
+                          ),
+                        ],
+                      ),
+                      Container(
+                          padding: const EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: theme.colorScheme.secondaryContainer,
+                          ),
+                          child: const Icon(Icons.edit)),
+                    ],
+                  ),
+                ),
+              ),
           ],
         ),
       )),
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,14 @@
-import 'package:class_manage/bottom_bar.dart';
+import 'package:class_manage/login.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -15,14 +21,13 @@ class MyApp extends StatelessWidget {
       title: 'Class manage',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: Colors.deepPurple,
-          brightness: Brightness.dark,
-        ),
-        useMaterial3: true,
-        textTheme: GoogleFonts.robotoMonoTextTheme()
-      ),
-      home: const BottomBar(),
+          colorScheme: ColorScheme.fromSeed(
+            seedColor: Colors.deepPurple,
+            brightness: Brightness.dark,
+          ),
+          useMaterial3: true,
+          textTheme: GoogleFonts.robotoMonoTextTheme()),
+      home: const LoginView(),
     );
   }
 }
